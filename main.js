a6cdd97d96f4cb934168df6f65d8914be266445e
Patrycja M <patrycja.michalek00@gmail.com>
2020-10-19 14:02:02 +0200

    make sections and images for each in mobile view

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const burgerMenu = document.querySelector('.burger-menu');
 const casualMenu = document.querySelector('.menu');
 const times = document.querySelector('.fa-times');
 
+const childs = document.querySelector('.menu ul').children;
+
 window.onresize = function(){ location.reload(); }
 burgerMenu.addEventListener('click', ()=>{
     casualMenu.style.display = "block";
@@ -12,4 +14,11 @@ times.addEventListener('click', ()=>{
     casualMenu.classList.remove('overlay');
     casualMenu.style.display = "none";
 })
+
+for(let i = 0; i < childs.length; i++){
+    childs[i].addEventListener('click', ()=>{
+        casualMenu.classList.remove('overlay');
+        casualMenu.style.display = "none";
+    })
+}
 
